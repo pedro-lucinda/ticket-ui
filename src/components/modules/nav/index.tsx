8cd90735b270af6e2bdaafad4fb8e49841384d0d
pedrolucinda <pedrolucinda2021@gmail.com>
2023-04-20 19:04:36 -0300

    fix: nav style

--- src/components/modules/nav/index.tsx
+++ src/components/modules/nav/index.tsx
@@ -14,32 +14,39 @@ export const NavBar = () => {
     <div
       className={classNames('flex justify-between items-center px-5 py-5 ', {
         'bg-purple900': pathName !== HOME_PATH,
-        'bg-transparent absolute md:max-screen-size mx-auto w-full top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2':
+        'bg-transparent absolute  w-full top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2':
           pathName === HOME_PATH,
       })}
     >
-      <img
-        src={LOGO}
-        alt="Ticket"
-        onClick={() => router.push(HOME_PATH)}
-        className="cursor-pointer"
-      />
-
-      <section className="flex gap-10 items-center">
-        {NAV_LINKS.map((link) => (
-          <NavLink
-            key={link.label}
-            title={link.label}
-            onClick={() => router.push(link.path)}
-          />
-        ))}
-        <Button
-          variant="purple"
-          label="Login"
-          size="sm"
-          onClick={() => router.push('/login')}
+      <div
+        className={classNames({
+          'flex justify-between items-center w-full bg-purple900 md:max-screen-size mx-auto':
+            pathName !== HOME_PATH,
+        })}
+      >
+        <img
+          src={LOGO}
+          alt="Ticket"
+          onClick={() => router.push(HOME_PATH)}
+          className="cursor-pointer"
         />
-      </section>
+
+        <section className="flex gap-10 items-center">
+          {NAV_LINKS.map((link) => (
+            <NavLink
+              key={link.label}
+              title={link.label}
+              onClick={() => router.push(link.path)}
+            />
+          ))}
+          <Button
+            variant="purple"
+            label="Login"
+            size="sm"
+            onClick={() => router.push('/login')}
+          />
+        </section>
+      </div>
     </div>
   )
 }
